b455f9c60192cee0ca1cc221d654aa682027630a
Niamh13 <77692255+Niamh13@users.noreply.github.com>
2025-11-14 11:19:21 +0000

    Update API URL to production endpoint

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,4 @@
-const apiUrl = "http://127.0.0.1:3000/patterns";
+const apiUrl = "ec2-44-206-128-223.compute-1.amazonaws.com/patterns";
 
 // Load patterns, optionally filtered by search term
 async function loadPatterns(searchTerm = "") {
